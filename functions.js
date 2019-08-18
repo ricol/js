aeb7397779036c4847df1077f824875bd724c0e7
Ricol Wang <ricol@opensimsim.com>
2019-08-18 22:44:27 +1000

    update

--- functions.js
+++ functions.js
@@ -139,3 +139,39 @@ showName("Julius", "Caesar");
 // shows: 1, Ilya, undefined (no second argument)
 showName("Ilya");
 }
+
+{
+  let sum = new Function('a', 'b', 'return a + b');
+
+alert( sum(1, 2) ); // 3
+}
+
+{
+  let sayHi = new Function('alert("Hello")');
+
+sayHi(); // Hello
+}
+
+{
+  function getFunc() {
+  let value = "test";
+
+  let func = new Function('alert(value)');
+
+  return func;
+}
+
+// getFunc()(); // error: value is not defined
+}
+
+{
+  function getFunc() {
+  let value = "test";
+
+  let func = function() { alert(value); };
+
+  return func;
+}
+
+getFunc()(); // "test", from the Lexical Environment of getFunc
+}
